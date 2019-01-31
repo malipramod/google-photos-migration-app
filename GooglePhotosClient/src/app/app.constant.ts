b84a3eedf93503d881d67b05a52fc613f0b0fc3a
Pramod Mali <pramodmaali@gmail.com>
2019-01-31 10:29:06 +0530

    Removed "plus.me" scope as API is shutting down
    https://developers.google.com/+/api-shutdown

--- GooglePhotosClient/src/app/app.constant.ts
+++ GooglePhotosClient/src/app/app.constant.ts
@@ -4,7 +4,7 @@ export const clientId:string ="62681126361-p2dmnds3qt7h1e52ds98po6eem5f4f7i.apps
 export const scope = [
     'profile',
     'email',
-    'https://www.googleapis.com/auth/plus.me',
+    // 'https://www.googleapis.com/auth/plus.me',
     'https://www.googleapis.com/auth/contacts.readonly',
     'https://www.googleapis.com/auth/admin.directory.user.readonly',
     'https://www.googleapis.com/auth/photoslibrary.readonly',
