c44a1ea110435885925683ee5481982b9b0cdc4d
Pramod Mali <pramodmaali@gmail.com>
2020-07-22 23:28:24 +0530

    Add Google login

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,17 +1,94 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
 import Container from '../Container';
 import AuthCard from './AuthCard';
 import { loginButtons } from '../../config/config';
 
+interface AuthUser {
+    token?: string;
+    id?: string;
+    expiresAt?: string;
+    email?: string;
+    image?: string;
+    name?: string;
+    loggedIn?: boolean;
+}
+
 export default function Auth() {
+    const [sourceUser, setSourceUser] = useState<AuthUser>({});
+    const [destinationUser, setDestinationUser] = useState<AuthUser>({});
+
+    useEffect(()=>{
+        const localStorageSource = JSON.parse(localStorage.getItem('GSource')!);
+        const localStorageDestination = JSON.parse(localStorage.getItem('GDestination')!);
+        setSourceUser(localStorageSource);
+        setDestinationUser(localStorageDestination);
+
+    },[])
+    function onSuccess(response: GoogleLoginResponse | GoogleLoginResponseOffline, type: string) {
+        const authResponse = (response as GoogleLoginResponse).getAuthResponse();
+        const profile = (response as GoogleLoginResponse).getBasicProfile();
+        const accountData: AuthUser = {
+            token: authResponse.access_token,
+            id: profile.getId(),
+            expiresAt: (new Date(authResponse.expires_at)).toString(),
+            email: profile.getEmail(),
+            image: profile.getImageUrl(),
+            name: profile.getName(),
+            loggedIn: true
+        }
+        if (type === "source"){
+            localStorage.setItem("GSource", JSON.stringify(accountData));
+            setSourceUser(accountData);
+        }else if (type === "destination"){
+            localStorage.setItem("GDestination", JSON.stringify(accountData));
+            setDestinationUser(accountData)
+        }
+    }
+
+    function onFailure(error: any){
+        console.log(error);
+    }
+    
+    function onLogoutSuccess(type: string){
+        if(type==="source"){
+            const newSourceUser = { ...sourceUser, loggedIn: false };
+            setSourceUser(newSourceUser);
+            localStorage.setItem("GSource", JSON.stringify(newSourceUser));
+        }else if(type==="destination"){
+            const newDestinationUser = { ...destinationUser, loggedIn: false };
+            setDestinationUser(newDestinationUser);
+            localStorage.setItem("GDestination", JSON.stringify(newDestinationUser));
+        }
+    }
+
+    function onLogoutFailure() {
+        console.log('Failed to logout');
+    }
+
     return (
         <Container>
             {loginButtons.map(loginButton => (
                 <AuthCard
                     key={loginButton.id}
-                    title={loginButton.title}
-                    subTitle={loginButton.subTitle}
+                    title={
+                        loginButton.type === "source" && sourceUser.loggedIn! ? sourceUser.name! :
+                        loginButton.type === "destination" && destinationUser.loggedIn! ? destinationUser.name! :
+                        loginButton.title
+                    }
+                    subTitle={
+                        loginButton.type === "source" && sourceUser.loggedIn! ? sourceUser.email! :
+                        loginButton.type === "destination" && destinationUser.loggedIn! ? destinationUser.email! :
+                        loginButton.subTitle
+                    }
+                    imageURL={loginButton.type === "source" ? sourceUser.image! : destinationUser.image!}
                     buttonText={loginButton.buttonText}
+                    type={loginButton.type}
+                    isLoggedIn={loginButton.type === "source" ? sourceUser.loggedIn! : destinationUser.loggedIn!}
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    onLogoutSuccess={onLogoutSuccess}
+                    onLogoutFailure={onLogoutFailure}
                 />
             ))}
         </Container>
--- src/components/Auth/AuthCard.tsx
+++ src/components/Auth/AuthCard.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Button, Card, Classes, Elevation, H5 } from "@blueprintjs/core";
+import { Card, Elevation, H5, Divider } from "@blueprintjs/core";
+import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline, GoogleLogout } from 'react-google-login';
+import { clientKey, scope } from '../../config/config';   
 
 interface AuthCardProps {
     title: string;
     subTitle: string;
     buttonText: string;
+    type: string;
+    isLoggedIn: boolean;
+    imageURL: string;
+    onSuccess: (response: GoogleLoginResponse | GoogleLoginResponseOffline, type: string) => void;
+    onFailure: (error: any) => void;
+    onLogoutSuccess: (type: string) => void;
+    onLogoutFailure: () => void;
 }
 
-export default function AuthCard({ title, subTitle, buttonText }: AuthCardProps) {
-    const cardStyle = { margin: '10px' };
+export default function AuthCard({ title, subTitle, buttonText, type, isLoggedIn, imageURL, onSuccess, onFailure, onLogoutSuccess, onLogoutFailure }: AuthCardProps) {
+    const cardStyle = { margin: '10px' };    
     return (
         <Card elevation={Elevation.THREE} style={cardStyle}>
             <H5>
@@ -17,7 +26,28 @@ export default function AuthCard({ title, subTitle, buttonText }: AuthCardProps)
             <p>
                 {subTitle}
             </p>
-            <Button text={buttonText} className={Classes.BUTTON} />
+            <Divider/>
+            <img width="200" height="200" src={imageURL} alt=""/>
+            <Divider/>
+            {
+                isLoggedIn?
+                (<GoogleLogout
+                    clientId={clientKey}
+                    buttonText={"Logout"}                    
+                    onLogoutSuccess={()=>onLogoutSuccess(type)}
+                    onFailure={onLogoutFailure}
+                />):
+                (<GoogleLogin
+                    clientId={clientKey}
+                    buttonText={buttonText}
+                    onSuccess={(response: GoogleLoginResponse | GoogleLoginResponseOffline)=>onSuccess(response, type)}
+                    onFailure={onFailure}
+                    cookiePolicy={ 'single_host_origin' }
+                    responseType='code,token'
+                    scope={scope}
+                />)
+            }
+            
         </Card>
     )
 }
