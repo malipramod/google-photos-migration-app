8a54c07d911764f3602d2fe4660f2b9befd41a02
Pramod Mali <pramodmaali@gmail.com>
2020-07-22 16:31:12 +0530

    Remove Angular

--- types/google-photos-migration.d.ts
+++
@@ -1,4 +0,0 @@
-export function createAlbum(albumName: any, authToken: any): any;
-export function getAlbums(authToken: any, nextPageToken: any): any;
-export function getAllItemsFromAlbum(album: any, authToken: any, nextPageToken: any): any;
-export function migrateAlbum(srcAuthToken: any, destAuthToken: any, albumData: any): any;
