34ba82b6275710b1c328d4312f2ee38453c86264
Pramod Mali <pramodmaali@gmail.com>
2020-07-22 21:51:20 +0530

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Auth from './components/Auth/Auth';
+import Migrate from './components/Migrate/Migrate';
+import Home from './components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header title="Google Photos Migration" />
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="/auth" >
+                        <Auth />
+                    </Route>
+                    <Route path="/migrate">
+                        <Migrate />
+                    </Route>
+
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
